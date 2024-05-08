77797652b74c60e7b8cbe785a48511de9be3010f
dhruva7254 <87358476+dhruva7254@users.noreply.github.com>
2024-05-08 14:07:07 +0530

    Add files via upload

---
+++ Files/08-05-2024/8_may_1.py
@@ -0,0 +1,135 @@
+# # import math
+# # import math
+# # print(math.factorial(4))
+# # print(math.sin(90))
+
+# import math
+# print(math.factorial(5))
+# print(math.gcd(12,18))
+# print(math.lcm(6,12))
+# print(math.perm(456,3))
+# print(math.comb(5,3))
+# print(math.perm(5,3))
+# print(math.sqrt(2))
+# print(math.pow(4,2))
+# print(math.log10(1))
+# var = 20
+# if var > 30:
+#     print("hi")
+# elif var == 20:
+#     print("ght")
+# else:
+#     print("ghj")
+# # for
+# # for else
+# # while
+# # while else
+# i=3 
+# if i < 5:
+#     pass
+# else:
+#     print("greater than 5")
+# print(1.5e200 *2.0e210)
+# print(1/3)
+# print(format(1/3, '.3f'))
+# print(format(1/3,'.3f'))
+# print(1/3 + 1/3 + 1/3 + 1/3 + 1/3 + 1/3)
+# print(6*1/3)
+# print(format(22/7,'.5f'))
+# print("fgh",format("Hello",'^15'),"ght")
+# print("fgh",format("Hello",'#<10'),"ght")
+# print(ord('a'))
+# print(ord('z'))
+# print(ord('\n'))
+# print(chr(99))
+# print("abc\nbcd")
+# print(1,'\n',2)
+# print("hello \n\n!!")
+# print("This is '\t' means tab")
+# print("This","is","different",sep='\t')
+# print("This","is","different",sep='#')
+# print("This","is","different",sep='\n') 
+# print("This","is","different",end='\n')
+# print(divmod(101,3))
+# print(8 << 1)
+# print(8 << 2)
+# print(32 >> 2)
+# """v1 = int(input("enrer marks: "))
+# print(v1)
+# v2 = int(input("enrer marks: "))
+# print(v2)
+# sde = 0
+# sde = v1 + v2
+# print(sde)"""
+# x=10
+# y=34.4
+# print(x and y)
+# print(x or y)
+# marks=[56,67,78,39,67]
+# for i in marks: 
+#     if i<50:
+#         print("fail")
+#         break
+# else:
+#     print("All pass")
+# for i in range(6):
+#     print(i)
+# tl = int(input())
+import re
+qwe = "dfg frt gft 9089675432 abf ght tyd fgr 8900670045"
+a = re.search('89',qwe)
+b = re.match('89',qwe)
+c = re.findall('89',qwe)
+print(a)
+print(b)
+print(c)
+print(a.span)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
