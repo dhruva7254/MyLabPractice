12e46de8491210b936ea8f38f9fc3512833c9a9f
dhruva7254 <87358476+dhruva7254@users.noreply.github.com>
2024-05-18 17:39:40 +0530

    Add files via upload

---
+++ Files/TestMyMathClass.java
@@ -0,0 +1,10 @@
+package com.may18.tester;
+import com.may18.coder.MyMathClass;
+public class TestMyMathClass 
+{
+	public static void main(String[] args) 
+	{
+		MyMathClass obj1 = new MyMathClass();
+		System.out.println("Add="+obj1.add(12,23));
+	}
+}
