e2d3248657fdcf968ade733ca0a31233da8f62df
dhruva7254 <87358476+dhruva7254@users.noreply.github.com>
2024-05-22 14:53:44 +0530

    Add files via upload

--- Files/Employee.java
+++ Files/Employee.java
@@ -1,31 +1,81 @@
-package com.bank;
-public class Employee 
-{
+package com.iacsd;
+
+public class Employee {
 	private int empid;
-	private String name;
-	private int age;
-	private static int count;
-	public Employee()
-	{
-		empid=1;
-		name="xyz";
-		age=30;
-		count++;	
+	
+	private String name,city,email;
+	private double salary;
+	public int getEmpid() {
+		return empid;
 	}
-	public Employee(int empid, String name, int age) 
-	{
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public Employee() {
+		super();
+	}
+	public Employee(int empid, String name, String city, String email, double salary) {
 		super();
 		this.empid = empid;
 		this.name = name;
-		this.age = age;
-		count++;
+		this.city = city;
+		this.email = email;
+		this.salary = salary;
 	}
-	public static void showCount()
+	@Override
+	public String toString() {
+		return "Employee [empid=" + empid + ", name=" + name + ", city=" + city + ", email=" + email + ", salary="
+				+ salary + "]";
+	}
+	
+	
+	
+	
+	public void calSalary( )
 	{
-		System.out.println("Count:"+count);
+		//BL
 	}
-	public void show()
+	
+	
+	public void applyLeave(int days )
 	{
-		System.out.println("EmpId="+empid+"   Name   "+name+"   "+age+"    Count:"+count);
+		//BL
 	}
+	
+	public void getBalanceLeaves( )
+	{
+		//BL
+	}
+	
+	
+	
+	
+	
+	
+	
+
 }
